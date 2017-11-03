b3ba8cab95d62a34b7a3d97741efa88fcc978a63
Morgan Zehnder <morganzehnder@gmail.com>
2017-11-03 08:59:31 -0700

    assignment 22 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,28 @@
 var pointsArray = document.getElementsByClassName('point');
 
- var animatePoints = function(points) {
+var revealPoint = function(point){
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
-             var revealPoint = function(index){
-                 points[index].style.opacity = 1;
-                 points[index].style.transform = "scaleX(1) translateY(0)";
-                 points[index].style.msTransform = "scaleX(1) translateY(0)";
-                 points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-             }
+var animatePoints = function(points) {
+   forEach(points, revealPoint);
+};
 
-             for (var i =0; i <points.length; i++){
-               revealPoint(i);
-             }
-        };
+animatePoints();
 
-         animatePoints();
+window.onload = function() {
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
 
-         window.onload = function() {
-           if (window.innerHeight > 950) {
-             animatePoints(pointsArray);
-            }
 
-           var sellingPoints = document.getElementsByClassName('selling-points')[0];
-           var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-             window.addEventListener('scroll', function(event) {
-               if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+
+ window.addEventListener('scroll', function(event) {
+   if (points.Array[0].getBoudingClientRect().top <= 500){
             animatePoints(pointsArray);
           }
-             });
-         }
+      });
+}
